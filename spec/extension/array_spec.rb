2dee5869b22aa44b27f946d05e9bee349508b08c
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-12-26 20:15:44 +0900

    Improve test

--- spec/extension/array_spec.rb
+++ spec/extension/array_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe Array do
+  KeyValue = Struct.new(:key, :value) do
+    def <=> (other)
+      self.key <=> other.key
+    end
+  end
+
+  let (:array) { [KeyValue.new(1, 1), KeyValue.new(1, 2), KeyValue.new(1, 3), KeyValue.new(1, 4), KeyValue.new(0, 10)] }
+  let (:expect) { [KeyValue.new(0, 10), KeyValue.new(1, 1), KeyValue.new(1, 2), KeyValue.new(1, 3), KeyValue.new(1, 4)] }
+
   describe "#stable_sort_by" do
     it 'sorts stably' do
       ['a', 'c', 'bd', 'fe', 'b'].sort_by { |x| x.length }.should_not eq ['a', 'c', 'b', 'bd', 'fe']
@@ -17,16 +26,16 @@ describe Array do
   end
 
   describe "#stable_sort" do
-    it 'sorts' do
-      ['a', 'c', 'bd', 'fe', 'b'].stable_sort.should eq ['a', 'b', 'bd', 'c', 'fe']
+    it 'sorts stably' do
+      array.sort.to_s.should_not eq expect.to_s
+      array.stable_sort.to_s.should eq expect.to_s
     end
   end
 
   describe "#stable_sort!" do
-    it 'sorts' do
-      arr = ['a', 'c', 'bd', 'fe', 'b']
-      arr.stable_sort!.should eq ['a', 'b', 'bd', 'c', 'fe']
-      arr.should eq ['a', 'b', 'bd', 'c', 'fe']
+    it 'sorts stably' do
+      array.stable_sort!.to_s.should eq expect.to_s
+      array.to_s.should eq expect.to_s
     end
   end
 end
--- spec/extension/enumrator_spec.rb
+++ spec/extension/enumrator_spec.rb
@@ -1,16 +1,26 @@
 require 'spec_helper'
 
 describe Enumerator do
+    KeyValue = Struct.new(:key, :value) do
+    def <=> (other)
+      self.key <=> other.key
+    end
+  end
+
+  let (:array) { [KeyValue.new(1, 1), KeyValue.new(1, 2), KeyValue.new(1, 3), KeyValue.new(1, 4), KeyValue.new(0, 10)] }
+  let (:expect) { [KeyValue.new(0, 10), KeyValue.new(1, 1), KeyValue.new(1, 2), KeyValue.new(1, 3), KeyValue.new(1, 4)] }
+
   describe "#stable_sort_by" do
     it 'sorts stably' do
-      ['a', 'c', 'bd', 'fe', 'b'].each.sort_by { |x| x.length }.should_not eq ['a', 'c', 'b', 'bd', 'fe'].each
+      ['a', 'c', 'bd', 'fe', 'b'].each.sort_by { |x| x.length }.should eq ['a', 'c', 'b', 'fe', 'bd']
       ['a', 'c', 'bd', 'fe', 'b'].each.stable_sort_by { |x| x.length }.should eq ['a', 'c', 'b', 'bd', 'fe']
     end
   end
 
   describe "#stable_sort" do
-    it 'sorts' do
-      ['a', 'c', 'bd', 'fe', 'b'].each.stable_sort.should eq ['a', 'b', 'bd', 'c', 'fe']
+    it 'sorts stably' do
+      array.each.sort.to_s.should_not eq expect.to_s
+      array.each.stable_sort.to_s.should eq expect.to_s
     end
   end
 end
