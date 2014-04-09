13a36e21ae233870f1fcdbd3d33abb0966b04821
cielavenir <cielartisan@gmail.com>
2014-04-09 15:17:11 +0900

    updated spec

--- spec/extension/array_spec.rb
+++ spec/extension/array_spec.rb
@@ -10,25 +10,11 @@ describe Array do
   let (:array) { [KeyValue.new(1, 1), KeyValue.new(1, 2), KeyValue.new(1, 3), KeyValue.new(1, 4), KeyValue.new(0, 10)] }
   let (:expect) { [KeyValue.new(0, 10), KeyValue.new(1, 1), KeyValue.new(1, 2), KeyValue.new(1, 3), KeyValue.new(1, 4)] }
 
-  describe "#stable_sort_by" do
-    it 'sorts stably' do
-      ['a', 'c', 'bd', 'fe', 'b'].sort_by { |x| x.length }.should_not eq ['a', 'c', 'b', 'bd', 'fe']
-      ['a', 'c', 'bd', 'fe', 'b'].stable_sort_by { |x| x.length }.should eq ['a', 'c', 'b', 'bd', 'fe']
-    end
-  end
-
   describe "#stable_sort_by!" do
     it 'sorts stably' do
       arr = ['a', 'c', 'bd', 'fe', 'b']
       arr.stable_sort_by! { |x| x.length }.should eq ['a', 'c', 'b', 'bd', 'fe']
       arr.should eq ['a', 'c', 'b', 'bd', 'fe']
-    end
-  end
-
-  describe "#stable_sort" do
-    it 'sorts stably' do
-      array.sort.to_s.should_not eq expect.to_s
-      array.stable_sort.to_s.should eq expect.to_s
     end
   end
 
--- spec/extension/enumerable_spec.rb
+++ spec/extension/enumerable_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Enumerator do
+describe Enumerable do
     KeyValue = Struct.new(:key, :value) do
     def <=> (other)
       self.key <=> other.key
@@ -10,14 +10,30 @@ describe Enumerator do
   let (:array) { [KeyValue.new(1, 1), KeyValue.new(1, 2), KeyValue.new(1, 3), KeyValue.new(1, 4), KeyValue.new(0, 10)] }
   let (:expect) { [KeyValue.new(0, 10), KeyValue.new(1, 1), KeyValue.new(1, 2), KeyValue.new(1, 3), KeyValue.new(1, 4)] }
 
-  describe "#stable_sort_by" do
+  describe "#stable_sort_by (Array)" do
     it 'sorts stably' do
-      ['a', 'c', 'bd', 'fe', 'b'].each.sort_by { |x| x.length }.should eq ['a', 'c', 'b', 'fe', 'bd']
-      ['a', 'c', 'bd', 'fe', 'b'].each.stable_sort_by { |x| x.length }.should eq ['a', 'c', 'b', 'bd', 'fe']
+      ['a', 'c', 'bd', 'fe', 'b'].sort_by { |x| x.length }.should_not eq ['a', 'c', 'b', 'bd', 'fe']
+      ['a', 'c', 'bd', 'fe', 'b'].stable_sort_by { |x| x.length }.should eq ['a', 'c', 'b', 'bd', 'fe']
+      ['a', 'c', 'bd', 'fe', 'b'].stable_sort_by.is_a?(Enumerator).should be_true
     end
   end
 
-  describe "#stable_sort" do
+  describe "#stable_sort_by (Enumerator)" do
+    it 'sorts stably' do
+      ['a', 'c', 'bd', 'fe', 'b'].each.sort_by { |x| x.length }.should eq ['a', 'c', 'b', 'fe', 'bd']
+      ['a', 'c', 'bd', 'fe', 'b'].each.stable_sort_by { |x| x.length }.should eq ['a', 'c', 'b', 'bd', 'fe']
+      ['a', 'c', 'bd', 'fe', 'b'].each.stable_sort_by.is_a?(Enumerator).should be_true
+    end
+  end
+
+  describe "#stable_sort (Array)" do
+    it 'sorts stably' do
+      array.sort.to_s.should_not eq expect.to_s
+      array.stable_sort.to_s.should eq expect.to_s
+    end
+  end
+
+  describe "#stable_sort (Enumerator)" do
     it 'sorts stably' do
       array.each.sort.to_s.should_not eq expect.to_s
       array.each.stable_sort.to_s.should eq expect.to_s
