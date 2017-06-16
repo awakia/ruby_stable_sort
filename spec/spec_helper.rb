18cc08695732835dde19b5d2b6dd61cc28681bd5
Wayne Conrad <kf7qga@gmail.com>
2017-06-16 15:34:33 -0700

    Make tests more reliable, especially for MRI >= 2.2
    * Monkey-patch built-sorts to force them to be unstable in all Rubies.
      Some rubies (such as MRI >= 2.2) have stable sorts, which can cause
      the tests to pass erroneously.  See
      https://stackoverflow.com/a/44486562/238886 for more about sort
      stability in Ruby
    
    * Set the random number seed in order to make tests repeatable.  Since
      the tests sort relatively short arrays, random chance would
      sometimes cause erroneous test failures.
    
    * Some tests checked that Ruby's built-in sort is unstable.  Those
      checks were moved into their own sections and expanded.  Now every
      Ruby built-in sort method is tested for to ensure that it is
      unstable.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,8 @@ Coveralls.wear!
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'stable_sort'
+
+glob = File.join(File.dirname(__FILE__), "support/**/*.rb")
+Dir[glob].sort.each do |path|
+  require path
+end
